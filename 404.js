2687638850217747c61b755ed00d9ecf3a61d0c6
aSwanting <gab.damico@gmail.com>
2024-01-25 01:11:37 +0100

    Updates

---
+++ 404.js
@@ -0,0 +1,7 @@
+function redirectWithQuery() {
+  const basePath = "https://aswanting.github.io/portfolio/?from=";
+  const pathQuery = window.location.pathname.split("/").at(-1);
+  window.location.href = basePath + pathQuery;
+}
+
+redirectWithQuery();
