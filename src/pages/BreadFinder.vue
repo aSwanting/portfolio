be376c2bc3d2e6ad3b254c8c6c452af781289f2b
aSwanting <gab.damico@gmail.com>
2024-01-31 19:51:47 +0100

    the quest for bread continues

--- src/pages/BreadFinder.vue
+++ src/pages/BreadFinder.vue
@@ -2,7 +2,12 @@
   <section>
     <div class="container">
       <div class="search-wrapper">
-        <input class="query-input" type="text" v-model="breadQuery" />
+        <input
+          class="query-input"
+          type="text"
+          v-model="breadQuery"
+          @keydown.enter="tomtomSearch()"
+        />
         <div v-for="result in queryResults">
           <div class="query-result">
             <span>{{ result.address.freeformAddress }}</span>
@@ -25,24 +30,76 @@ export default {
       breadMap: breadMap,
       API_URL: "https://api.tomtom.com/search/2",
       API_KEY: "qD5AjlcGdPMFjUKdDAYqT7xYi3yIRo3c",
-      API_TYPE: "/search/",
       API_EXT: ".json",
+      queryType: null,
       breadList: null,
       breadQuery: null,
       queryResults: null,
     };
   },
   watch: {
-    breadQuery() {
-      this.tomtomSearch();
+    queryResults() {
+      this.tomtomFilter();
     },
+    // breadQuery() {
+    //   this.tomtomSearch();
+    //   // this.tomtomFilter();
+    // },
   },
   methods: {
+    tomtomFilter() {
+      this.queryType = "/geometryFilter";
+      const filterURL = this.API_URL + this.queryType + this.API_EXT;
+      const lat = this.queryResults[0].position.lat;
+      const lon = this.queryResults[0].position.lon;
+      const searchRadius = 100;
+
+      console.log(`Filter:
+      Filter URL: ${filterURL}
+      Search Position: ${lat} ${lon}
+      Search Radius: ${searchRadius} meters
+      `);
+
+      const geometryList = [
+        {
+          type: "CIRCLE",
+          position: lat + " " + lon,
+          radius: searchRadius,
+        },
+      ];
+
+      const poiList = [
+        {
+          lat: "37.61274",
+          lon: "15.166195",
+        },
+        {
+          lat: "37.61274",
+          lon: "15.168195",
+        },
+        {
+          lat: "38.61274",
+          lon: "12.166195",
+        },
+      ];
+
+      console.log(JSON.stringify(geometryList));
+      console.log(JSON.stringify(poiList));
+
+      axios.post(`${filterURL}`, {
+        params: {
+          // geometryList: JSON.stringify(geometryList),
+          // poiList: JSON.stringify(poiList),
+          key: this.API_KEY,
+        },
+      });
+    },
     tomtomSearch() {
+      this.queryType = "/search/";
       if (this.breadQuery) {
         axios
           .get(
-            `${this.API_URL}${this.API_TYPE}${this.breadQuery}${this.API_EXT}`,
+            `${this.API_URL}${this.queryType}${this.breadQuery}${this.API_EXT}`,
             {
               params: {
                 key: this.API_KEY,
@@ -50,29 +107,11 @@ export default {
             }
           )
           .then((res) => {
-            console.log(res.data);
+            // console.log(res.data);
             this.queryResults = res.data.results;
           });
       }
     },
-  },
-  mounted() {
-    // console.log(this.breadMap.slice(0, 10));
-    // axios
-    //   .get(this.API_URL, {
-    //     params: {
-    //       key: this.API_KEY,
-    //       geometryList: {
-    //         type: "CIRCLE",
-    //         position: "40.80558, -73.96548",
-    //         radius: 100,
-    //       },
-    //       poiList: this.breadMap.slice(1, 10),
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //   });
   },
 };
 </script>
